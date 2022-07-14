f89ba63353cb8507291a14092fb3089782764059
Adrian <adriangiulianotucci@gmail.com>
2022-07-13 22:43:56 -0300

    add public and private keys env

--- keys/private-key.js
+++ keys/private-key.js
@@ -3,7 +3,7 @@ const path = require("path");
 const config = require("config");
 
 const privateKey = fs.readFileSync(
-  path.join(__dirname, `./${config.auth.key}.pk`)
+  path.join(__dirname, `./${config.auth.key}.pk`) || process.env.BAR_PK
 );
 
 module.exports = privateKey;
--- keys/public-key.js
+++ keys/public-key.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const config = require("config");
 
-const publicKey = fs.readFileSync(
-  path.join(__dirname, `./${config.auth.key}.pub`)
-);
+const publicKey =
+  fs.readFileSync(path.join(__dirname, `./${config.auth.key}.pub`)) ||
+  process.env.BAR_PUB;
 
 module.exports = publicKey;
