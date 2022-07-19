865ba4cb158f6b2c212ca3c3716fb19cfdbe7020
Adrian <adriangiulianotucci@gmail.com>
2022-07-19 20:00:04 -0300

    new approach to access keys

--- keys/private-key.js
+++ keys/private-key.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const config = require("config");
 
-const privateKeyPath =
-  process.env.PRIVATE_KEY_PATH || config.auth.privateKeyPath;
+// const privateKeyPath = process.env.PRIVATE_KEY_PATH || config.auth.privateKeyPath;
 
-const privateKey = fs.readFileSync(__basedir + privateKeyPath);
+// const privateKey = fs.readFileSync(__basedir + privateKeyPath);
+const privateKey = "";
 
 module.exports = privateKey;
--- keys/public-key.js
+++ keys/public-key.js
@@ -2,8 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const config = require("config");
 
-const publicKeyPath = process.env.PUBLIC_KEY_PATH || config.auth.privateKeyPath;
+// const publicKeyPath = process.env.PUBLIC_KEY_PATH || config.auth.privateKeyPath;
 
-const publicKey = fs.readFileSync(__basedir + publicKeyPath);
+// const publicKey = fs.readFileSync(__basedir + publicKeyPath);
+const publicKey = "";
 
 module.exports = publicKey;
